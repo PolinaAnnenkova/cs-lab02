881eeee2f6438c21e9e9d57a6df0c7b8a05bbaf9
Alice (AnnenkovaPR) <polineromanovna@mail.ru>
2023-06-30 11:16:09 +0300

    New Type

--- project/project.cpp
+++ project/project.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B:";
-    int a, b;
+    float a, b;
     cin >> a >> b;
     cout << "A+B=" << a + b << '\n';
     cout << "A-B=" << a - b << '\n';
